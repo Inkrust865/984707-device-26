020dca008b4a38c7a59eea53bd09ed93f25a624e
AntonenkoN <antonenko_natash@mail.ru>
2019-11-04 19:21:40 +0300

    Исправление ошибок

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,26 @@
+var modalMap = document.querySelector(".modal-map");
+var buttonMap = document.querySelector(".button-map");
+var mapClose = modalMap.querySelector(".button-close");
+
+buttonMap.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	modalMap.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	modalMap.classList.remove("modal-show");
+})
+
+window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		if (modalMap.classList.contains("modal-show")) {
+			evt.preventDefault();
+			modalMap.classList.remove("modal-show");
+		}
+	}
+})
+
 var modalFeedback = document.querySelector(".modal-feedback");
 var buttonFeedback = document.querySelector(".button-feedback");
 var buttonClose = modalFeedback.querySelector(".button-close");
